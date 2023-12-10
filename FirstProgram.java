21a5ee9c211b943fbd9f241b9cea093ace591a8c
sheverdyaeva@gmail.com+1-at-926736938067 <sheverdyaeva@gmail.com>
2023-12-10 00:30:31 -0500

    new commit

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram{
+
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!\n" + 
+                           "Java > Python\n" +
+                           "I spilled Java all over my paper.\n" +
+                           "My dog ate my Java.");
+    }
+}
